c4a041f46c5ee15f2a8de2b195e649ddf6c5f380
PhatCT24 <tienphat0812004@gmail.com>
2024-11-23 21:34:17 +0700

    update basic form

---
+++ MenuForm.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class MenuForm {
+    private JComboBox comboBox1;
+}
---
+++ src/Login.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Login {
+    private JTextField username;
+    private JPasswordField password;
+    private JButton loginbutton;
+}
